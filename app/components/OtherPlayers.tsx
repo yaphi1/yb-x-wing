97bb77c31b11e73995abb0d6010e15694052114e
Yaphi Berhanu <yaphi1@gmail.com>
2024-10-21 17:40:42 -0400

    Clean up lint

--- app/components/OtherPlayers.tsx
+++ app/components/OtherPlayers.tsx
@@ -2,25 +2,18 @@ import * as THREE from 'three';
 import { useOthers } from '@liveblocks/react/suspense';
 import { useFrame } from '@react-three/fiber';
 import { XWing } from './XWing';
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
 import { type Presence } from '../helpers/multiplayerConfig';
 
 type NestedRef = React.MutableRefObject<THREE.Group>;
 const emptyRef = { current: null! };
 
-/** Default value from LiveBlocks measured in seconds */
-const secondsBetweenWebsocketTransmissions = 0.1;
-
 export function OtherPlayers() {
   const otherPlayers = useOthers();
   const otherXWings = useRef<Record<string, NestedRef>>({});
-  const player2Ref = useRef<THREE.Group>(null!);
 
-  const frameCount = useRef(0);
-  const frameCountAtTransmission = useRef(0);
   const previousTransmission = useRef<Presence>(null!);
   const latestTarget = useRef<Presence>(null!);
-  const framesBetweenSocketTransmissions = useRef(0);
 
   const previousTransmissionTimestamp = useRef(0);
   const secondsBetweenTransmissions = useRef(0.1);
@@ -32,32 +25,30 @@ export function OtherPlayers() {
       const otherPlayerPresence = otherPlayer.presence as Presence;
       const otherPlayerXWing = otherXWings.current[otherPlayer.connectionId].current;
 
-      /*
-      order of operations:
-      - transmission A comes in
-      - we have secondsBetweenTransmissions seconds to lerp to it
-      - transmission B comes in
-      - transmission A becomes previous
-      - we lerp from A to B
+      /**
+        order of operations:
+        - transmission A comes in
+        - we have secondsBetweenTransmissions seconds to lerp to it
+        - transmission B comes in
+        - transmission A becomes previous
+        - we lerp from A to B
       */
-
       const hasNewTransmission = otherPlayerPresence !== previousTransmission.current;
 
       if (hasNewTransmission) {
-        // transmission has arrived
-        latestTarget.current = otherPlayerPresence; // latest target
-        previousPosition.current = otherPlayerXWing.position; // actual position
+        latestTarget.current = otherPlayerPresence;
+        previousPosition.current = otherPlayerXWing.position;
 
         const { elapsedTime } = state.clock;
         secondsBetweenTransmissions.current = (elapsedTime - previousTransmissionTimestamp.current) / 1000;
         previousTransmissionTimestamp.current = secondsBetweenTransmissions.current;
       }
 
-      const fractionOfLerpCompletedPerFrame = delta / secondsBetweenTransmissions.current;
+      // const fractionOfLerpCompletedPerFrame = delta / secondsBetweenTransmissions.current;
 
-      window.fractionOfLerpCompletedPerFrame = fractionOfLerpCompletedPerFrame;
-      window.delta = delta;
-      window.secondsBetweenTransmissions = secondsBetweenTransmissions.current;
+      // window.fractionOfLerpCompletedPerFrame = fractionOfLerpCompletedPerFrame;
+      // window.delta = delta;
+      // window.secondsBetweenTransmissions = secondsBetweenTransmissions.current;
 
       otherPlayerXWing.position.lerpVectors(
         previousPosition.current,
@@ -72,25 +63,7 @@ export function OtherPlayers() {
       otherPlayerXWing.quaternion.slerp(new THREE.Quaternion(...otherPlayerPresence.quaternion), 0.5);
 
       // otherPlayerXWing.position.set(...otherPlayerPresence.position);
-
     });
-
-
-
-    /* otherPlayers.forEach((otherPlayer) => {
-      const otherPlayerPresence = otherPlayer.presence as Presence;
-      const otherPlayerXWing = otherXWings.current[otherPlayer.connectionId].current;
-
-      window.position = otherPlayerXWing.position;
-      window.positionFromPresence = otherPlayerPresence.position;
-      window.otherXWings = otherXWings;
-  
-      // otherPlayerXWing.position.lerp(new THREE.Vector3(...otherPlayerPresence.position), fractionOfLerpCompletedPerFrame);
-      otherPlayerXWing.quaternion.slerp(new THREE.Quaternion(...otherPlayerPresence.quaternion), fractionOfLerpCompletedPerFrame);
-
-      otherPlayerXWing.position.set(...otherPlayerPresence.position);
-      // otherPlayerXWing.quaternion.set(...otherPlayerPresence.quaternion);
-    }); */
 
   });
 
@@ -104,10 +77,6 @@ export function OtherPlayers() {
           <XWing />
         </group>
       ))}
-
-      {/* <group ref={player2Ref}>
-        <XWing />
-      </group> */}
     </>
   );
 }
