955f97b8ec2bad623b40f3bad51220ac64d9534e
Yaphi Berhanu <yaphi1@gmail.com>
2024-10-29 02:00:24 -0400

    Add stats component to check framerate

--- app/components/Experience.tsx
+++ app/components/Experience.tsx
@@ -5,15 +5,17 @@ import { Canvas } from '@react-three/fiber';
 import { Environment } from '@react-three/drei';
 import { Keyboard } from './Keyboard';
 import { Player } from './Player';
-import { isMultiplayerEnabled } from '../helpers/globalFlags';
+import { isDebugMode, isMultiplayerEnabled } from '../helpers/globalFlags';
 import { OtherPlayers } from './OtherPlayers';
 import { Ground } from './Ground';
 import { Fog } from './Fog';
+import { Stats } from '@react-three/drei';
 
 export function Experience() {
   return (
     <Keyboard>
       <Canvas>
+        {isDebugMode && <Stats />}
         <color args={ [ '#ffe8e8' ] } attach="background" />
         <Environment preset="dawn" />
         <Fog color="#ffe8e8" near={200} far={2000} />
--- app/helpers/globalFlags.ts
+++ app/helpers/globalFlags.ts
@@ -1,2 +1,2 @@
-export const isDebugMode = false;
+export const isDebugMode = true;
 export const isMultiplayerEnabled = false; // keep this false most of the time to avoid blowing through free Liveblocks plan limits
